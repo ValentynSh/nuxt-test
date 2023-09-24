f3ed184efeb392860c4243efcf56a052519590b6
ValentynSh <92866788+ValentynSh@users.noreply.github.com>
2023-09-24 11:31:40 +0300

    13

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,10 +1,19 @@
 import express, { Router } from 'express';
 import serverless from 'serverless-http';
-
+import cors from 'cors'
+import { createUserHandler, findUsersHandler } from "../../src/controller/user.controller";
 const api = express();
 
 const router = Router();
 router.get('/hello', (req, res) => res.send('Hello World!'));
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+
+router.get("/user", cors(corsOptions),  findUsersHandler);
 
 api.use('/api/', router);
 
